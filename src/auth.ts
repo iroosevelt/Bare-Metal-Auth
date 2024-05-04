336fc576a00413e62614632b9cb382236225f0c3
iroosevelt <chat.roosevelt@gmail.com>
2024-05-04 16:31:42 +0100

    update: credentials

--- src/auth.ts
+++ src/auth.ts
@@ -1,11 +1,35 @@
-import db from "@/app/utils/db";
 import { PrismaAdapter } from "@auth/prisma-adapter";
+
 import NextAuth from "next-auth";
-import GitHub from "next-auth/providers/github";
-import Google from "next-auth/providers/google";
-import authConfig from "./auth.config";
+import db from "@/app/utils/db";
+import authConfig from "@/auth.config";
+import { getUserById } from "@/data/user";
+import { UserRole } from "@prisma/client";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
+  callbacks: {
+    async session({ token, session }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+
+      if (token.role && session.user) {
+        session.user.role = token.role as UserRole;
+      }
+
+      return session;
+    },
+
+    async jwt({ token }) {
+      if (!token.sub) return token;
+
+      const existingUser = await getUserById(token.sub);
+
+      if (!existingUser) return token;
+
+      return token;
+    },
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   ...authConfig,
